753d70f3fd0841a74670d5cfd722eb4b798ef46f
王庆磊 <qinglei.wang@daydaycook.com>
2018-06-20 19:02:31 +0800

    迭代

--- utils/util.js
+++ utils/util.js
@@ -1,8 +1,8 @@
-let status = 0;    // 0 开发环境  1 测试环境  2 staging环境  3生产环境
+let status = 1;    // 0 开发环境  1 测试环境  2 staging环境  3生产环境
 
-let ajaxUrl  = status==0?'https://tv-d.daydaycook.com.cn':status==1?'https://tv-t.daydaycook.com.cn':status==2?'https://tv-s.daydaycook.com.cn':'https://tv.daydaycook.com.cn';              //用户、地址
+let ajaxUrl  = status==0?'https://tv-d.daydaycook.com.cn/':status==1?'https://tv-t.daydaycook.com.cn/':status==2?'https://tv-s.daydaycook.com.cn/':'https://tv.daydaycook.com.cn/';              //用户、地址
 let ajaxUrl2 = status==0?'https://uc-api-d.daydaycook.com.cn':status==1?'https://uc-api-t.daydaycook.com.cn':status==2?'https://uc-api-s.daydaycook.com.cn':'https://uc-api.daydaycook.com.cn';              //用户、地址
-ajaxUrl = "http://xuqing.natapp1.cc/";
+// ajaxUrl = "http://xuqing.natapp1.cc/";
 //通用Ajax请求接口
 let fetch = (_url,params,type) => {
     return new Promise(resolve => {
